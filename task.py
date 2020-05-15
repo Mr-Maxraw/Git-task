67a1a9f0d35811d0f03dbdb73ae8e3fd66ac16f6
Петухов Максим <maxraw111@gmail.com>
2020-05-15 13:37:17 +0500

    hooray

--- task.py
+++ task.py
@@ -20,23 +20,26 @@ for i in range(0, a):
 q = []
 q.append([int(sr), int(sc)])
 #print(q)
+used = []
+for i in range(0, a):
+    used.append([])
+    for j in range(0, a):
+        used[i].append(False)
 while q:
-    if (map[q[0][0]][q[0][1]] == int(newColor)):
-        q.pop()
-        continue
     if (q[0][0] > 0):
-        if (map[q[0][0]][q[0][1]] == map[q[0][0] - 1][q[0][1]]):
+        if (map[q[0][0]][q[0][1]] == map[q[0][0] - 1][q[0][1]] and not used[q[0][0] - 1][q[0][1]]):
             q.append([q[0][0] - 1, q[0][1]])
     if (q[0][1] > 0):
-        if (map[q[0][0]][q[0][1]] == map[q[0][0]][q[0][1] - 1]):
+        if (map[q[0][0]][q[0][1]] == map[q[0][0]][q[0][1] - 1] and not used[q[0][0]][q[0][1] - 1]):
             q.append([q[0][0], q[0][1] - 1])
     if (q[0][0] < a -1):
-        if (map[q[0][0]][q[0][1]] == map[q[0][0] + 1][q[0][1]]):
+        if (map[q[0][0]][q[0][1]] == map[q[0][0] + 1][q[0][1]] and not used[q[0][0] + 1][q[0][1]]):
             q.append([q[0][0] + 1, q[0][1]])
     if (q[0][1] < a -1):
-        if (map[q[0][0]][q[0][1]] == map[q[0][0]][q[0][1] + 1]):
+        if (map[q[0][0]][q[0][1]] == map[q[0][0]][q[0][1] + 1] and not used[q[0][0]][q[0][1] + 1]):
             q.append([q[0][0], q[0][1] + 1])
     map[q[0][0]][q[0][1]] = int(newColor)
+    used[q[0][0]][q[0][1]] = True
     print(q[0][0], q[0][1])
     q.pop(0)
 print(map)
